e15ccc71084d42b908bbd858ec2913bb6baf1087
Kristian Babulev <kriso4os@abv.bg>
2018-12-12 16:07:03 +0200

    Add project files.

---
+++ HelloWorld/Controllers/GreetingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+
+using static HelloWorld.Models.GreetingModels;
+
+namespace HelloWorld.Controllers
+{
+    public class GreetingController : Controller
+    {
+        Greeting myGreeting = new Greeting();
+        Greeting nameGreeting = new Greeting();
+
+        // GET: Default
+
+        public ActionResult Index(String id)
+        {
+            // Get greeting from Model and pass on to View via ViewData
+            ViewData["Greeting"] = myGreeting.getGreeting(id);
+            // Invoke the View
+            return View();
+        }
+
+        public ActionResult SayHello(String id)
+        {
+            // Get greeting from Model and pass on to View via ViewData
+            ViewData["Hello"] = nameGreeting.getNameGreeting(id);
+            // Invoke the View
+            return View();
+        }
+    }
+}
---
+++ HelloWorld/Models/GreetingModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HelloWorld.Models
+{
+    public class GreetingModels
+    {
+        public class Greeting
+        {
+            private String[] greeting = new String[] { "Hello World!", "Zdravei Sviat!" };
+            public String getGreeting(String option)
+            {
+                switch (option)
+                {
+                    case "English":
+                        return greeting[0];
+                    case "Bulgarian":
+                        return greeting[1];
+                    default:
+                        return "Please specify a language - English or Bulgarian";
+                }
+            }
+            public String getNameGreeting(String name)
+            {
+                if (name != null)
+                {
+                    return "Hello "+name;
+                }
+                else
+                {
+                    return "Who might you be?";
+                }
+            }
+            
+        }
+    }
+}
